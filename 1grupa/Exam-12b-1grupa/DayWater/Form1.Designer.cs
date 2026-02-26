03e052985bcfc4b78802ab40e4bb27b4978e95da
Mehmed Ali Mehmed <mases@abv.bg>
2026-02-26 16:09:07 +0200

    Add files via upload

---
+++ 1grupa/Exam-12b-1grupa/DayWater/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace DayWater
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            label4 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            checkBox1 = new CheckBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(750, 38);
+            label1.TabIndex = 0;
+            label1.Text = "Калкулатор за изчисляване на дневен прием на вода";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(12, 76);
+            label2.Name = "label2";
+            label2.Size = new Size(232, 38);
+            label2.TabIndex = 1;
+            label2.Text = "Въведете тегло:";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            numericUpDown1.Location = new Point(323, 76);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.ReadOnly = true;
+            numericUpDown1.Size = new Size(148, 43);
+            numericUpDown1.TabIndex = 2;
+            numericUpDown1.Value = new decimal(new int[] { 50, 0, 0, 0 });
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.Location = new Point(12, 135);
+            label3.Name = "label3";
+            label3.Size = new Size(284, 76);
+            label3.TabIndex = 3;
+            label3.Text = "Ниво на физическа\r\nактивност:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Малка/Никаква", "Умерена (1-3 пъти седмично)", "Висока (4+ пъти седмично)" });
+            comboBox1.Location = new Point(323, 153);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(431, 45);
+            comboBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.Location = new Point(12, 232);
+            label4.Name = "label4";
+            label4.Size = new Size(80, 38);
+            label4.TabIndex = 5;
+            label4.Text = "Пол:";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            radioButton1.Location = new Point(127, 232);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(107, 42);
+            radioButton1.TabIndex = 6;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Мъж";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            radioButton2.Location = new Point(271, 229);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(113, 42);
+            radioButton2.TabIndex = 7;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Жена";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            checkBox1.Location = new Point(431, 232);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(325, 42);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "Бременна/Кърмачка";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.Visible = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Green;
+            button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 373);
+            button1.Name = "button1";
+            button1.Size = new Size(213, 65);
+            button1.TabIndex = 14;
+            button1.Text = "Пресметни";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Cyan;
+            button2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.Location = new Point(277, 373);
+            button2.Name = "button2";
+            button2.Size = new Size(213, 65);
+            button2.TabIndex = 15;
+            button2.Text = "Изчисти";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Red;
+            button3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(542, 373);
+            button3.Name = "button3";
+            button3.Size = new Size(213, 65);
+            button3.TabIndex = 16;
+            button3.Text = "Изход";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.Location = new Point(155, 300);
+            label5.Name = "label5";
+            label5.Size = new Size(141, 38);
+            label5.TabIndex = 17;
+            label5.Text = "Резултат:";
+            label5.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(773, 450);
+            Controls.Add(label5);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(checkBox1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label4);
+            Controls.Add(comboBox1);
+            Controls.Add(label3);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown1;
+        private Label label3;
+        private ComboBox comboBox1;
+        private Label label4;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private CheckBox checkBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label5;
+    }
+}
---
+++ 1grupa/Exam-12b-1grupa/DayWater/Form1.cs
@@ -0,0 +1,51 @@
+﻿namespace DayWater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Value = 50;
+            comboBox1.SelectedIndex = -1;
+            checkBox1.Checked = false;
+            checkBox1.Visible = false;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            label5.Text = "";
+            label5.Visible = false;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Visible = true;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int recommend = int.Parse(numericUpDown1.Value.ToString()) * 35;
+            switch (comboBox1.SelectedIndex)
+            {
+                case 1: recommend += 300; break;
+                case 2: recommend += 600; break;
+            }
+            if (checkBox1.Checked) recommend += 700;
+            label5.Text = $"Препоръчителен прием: {recommend} мл.";
+            label5.Visible = true;
+        }
+    }
+}
