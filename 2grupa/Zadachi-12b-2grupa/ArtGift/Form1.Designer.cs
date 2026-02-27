6cc0cf8eb1947fb80f907b7fcfea83b81cdfd851
Mehmed Ali Mehmed <mases@abv.bg>
2026-02-27 09:56:31 +0200

    Add files via upload

---
+++ 2grupa/Zadachi-12b-2grupa/ArtGift/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace ArtGift
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            comboBox1 = new ComboBox();
+            numericUpDown1 = new NumericUpDown();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            label8 = new Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(199, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(857, 38);
+            label1.TabIndex = 1;
+            label1.Text = "Система за поръчка на персонализирани подаръци \"ArtGift\"";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(12, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(215, 38);
+            label2.TabIndex = 2;
+            label2.Text = "Въведете име:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.Location = new Point(12, 120);
+            label3.Name = "label3";
+            label3.Size = new Size(237, 38);
+            label3.TabIndex = 3;
+            label3.Text = "Въведете адрес:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.Location = new Point(12, 174);
+            label4.Name = "label4";
+            label4.Size = new Size(272, 38);
+            label4.TabIndex = 4;
+            label4.Text = "Изберете продукт:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.Location = new Point(12, 228);
+            label5.Name = "label5";
+            label5.Size = new Size(186, 38);
+            label5.TabIndex = 5;
+            label5.Text = "Количество:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label6.Location = new Point(12, 282);
+            label6.Name = "label6";
+            label6.Size = new Size(204, 38);
+            label6.TabIndex = 6;
+            label6.Text = "Тип доставка:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label7.Location = new Point(12, 369);
+            label7.Name = "label7";
+            label7.Size = new Size(327, 38);
+            label7.TabIndex = 7;
+            label7.Text = "Допълнителни опции:";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(356, 66);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(330, 43);
+            textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox2.Location = new Point(356, 120);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(330, 43);
+            textBox2.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Чаша - 5 евро", "Тениска - 10 евро", "Плакат - 8 евро" });
+            comboBox1.Location = new Point(356, 174);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(330, 45);
+            comboBox1.TabIndex = 10;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            numericUpDown1.Location = new Point(356, 228);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.ReadOnly = true;
+            numericUpDown1.Size = new Size(330, 43);
+            numericUpDown1.TabIndex = 11;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            radioButton1.Location = new Point(356, 282);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(296, 35);
+            radioButton1.TabIndex = 12;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Стандартна (безплатна)";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            radioButton2.Location = new Point(356, 323);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(258, 35);
+            radioButton2.TabIndex = 13;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Експресна (+3 евро)";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            checkBox1.Location = new Point(356, 369);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(361, 35);
+            checkBox1.TabIndex = 14;
+            checkBox1.Text = "Луксозна опаковка (+2 евро)";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            checkBox2.Location = new Point(356, 410);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(426, 35);
+            checkBox2.TabIndex = 15;
+            checkBox2.Text = "Поздравителна картичка (+1 евро)";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            listBox1.FormattingEnabled = true;
+            listBox1.HorizontalScrollbar = true;
+            listBox1.ItemHeight = 28;
+            listBox1.Location = new Point(709, 66);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(497, 228);
+            listBox1.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 192, 0);
+            button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 454);
+            button1.Name = "button1";
+            button1.Size = new Size(335, 65);
+            button1.TabIndex = 17;
+            button1.Text = "Изчисли и добави";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Cyan;
+            button2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.Location = new Point(545, 454);
+            button2.Name = "button2";
+            button2.Size = new Size(231, 65);
+            button2.TabIndex = 18;
+            button2.Text = "Изчисти";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Red;
+            button3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(974, 454);
+            button3.Name = "button3";
+            button3.Size = new Size(231, 65);
+            button3.TabIndex = 19;
+            button3.Text = "Изход";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label8.Location = new Point(821, 323);
+            label8.Name = "label8";
+            label8.Size = new Size(274, 38);
+            label8.TabIndex = 20;
+            label8.Text = "Обща крайна цена";
+            label8.Visible = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1218, 533);
+            Controls.Add(label8);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private ComboBox comboBox1;
+        private NumericUpDown numericUpDown1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label8;
+    }
+}
---
+++ 2grupa/Zadachi-12b-2grupa/ArtGift/Form1.cs
@@ -0,0 +1,70 @@
+﻿namespace ArtGift
+{
+    public partial class Form1 : Form
+    {
+        decimal total = 0;
+        string order = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            numericUpDown1.Value = 0;
+            comboBox1.SelectedIndex = -1;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            label8.Text = "";
+            label8.Visible = false;
+            listBox1.Items.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Моля, въведете име на клиента!", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(textBox2.Text))
+            {
+                MessageBox.Show("Моля, въведете адрес за доставка!", "Грешка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            decimal price = 0;
+            string product = comboBox1.SelectedItem.ToString();
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0: price += 5; break; 
+                case 1: price += 10; break; 
+                case 2: price += 8; break; 
+            }
+            int quantity = int.Parse(numericUpDown1.Value.ToString());
+            decimal medium = price * quantity;
+            if (checkBox1.Checked) medium += 2;
+            if (checkBox2.Checked) medium += 1;
+            if (radioButton2.Checked) medium += 3;
+            if (quantity > 10)
+            {
+                medium *= 0.9m;
+                MessageBox.Show("Приложена е 10% отстъпка за количество!");
+            }
+            total += medium;
+            string deliveryType = radioButton2.Checked ? "Експресна" : "Стандартна";
+            order = $"{textBox1.Text} | {product} * {quantity} броя | Доставка: {deliveryType}";
+            listBox1.Items.Add(order);
+            label8.Text = $"Обща крайна цена:\r\n{total:f2} евро";
+            label8.Visible = true;
+        }
+    }
+}
