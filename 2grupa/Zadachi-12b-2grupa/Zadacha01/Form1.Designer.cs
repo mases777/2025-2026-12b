2575d233aad495200a2682ef59bbefd3dac0eb8f
Mehmed Ali Mehmed <mases@abv.bg>
2026-02-27 09:52:16 +0200

    Add files via upload

---
+++ 2grupa/Zadachi-12b-2grupa/Zadacha01/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Zadacha01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(193, 41);
+            label1.TabIndex = 0;
+            label1.Text = "X кв.м. лозе";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(12, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(286, 41);
+            label2.TabIndex = 1;
+            label2.Text = "Y грозде за 1 кв.м.";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label3.Location = new Point(12, 133);
+            label3.Name = "label3";
+            label3.Size = new Size(314, 41);
+            label3.TabIndex = 2;
+            label3.Text = "Z нужни литра вино";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label4.Location = new Point(12, 193);
+            label4.Name = "label4";
+            label4.Size = new Size(262, 41);
+            label4.TabIndex = 3;
+            label4.Text = "Брой работници";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox1.Location = new Point(369, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(419, 47);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox2.Location = new Point(369, 71);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(419, 47);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox3.Location = new Point(369, 133);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(419, 47);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            textBox4.Location = new Point(369, 193);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(419, 47);
+            textBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label5.Location = new Point(12, 271);
+            label5.Name = "label5";
+            label5.Size = new Size(118, 41);
+            label5.TabIndex = 8;
+            label5.Text = "Label 5";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label6.Location = new Point(12, 321);
+            label6.Name = "label6";
+            label6.Size = new Size(118, 41);
+            label6.TabIndex = 9;
+            label6.Text = "Label 6";
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(0, 192, 0);
+            button1.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(12, 384);
+            button1.Name = "button1";
+            button1.Size = new Size(231, 65);
+            button1.TabIndex = 10;
+            button1.Text = "Пресметни";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Cyan;
+            button2.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button2.Location = new Point(284, 384);
+            button2.Name = "button2";
+            button2.Size = new Size(231, 65);
+            button2.TabIndex = 11;
+            button2.Text = "Изчисти";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Red;
+            button3.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(556, 384);
+            button3.Name = "button3";
+            button3.Size = new Size(231, 65);
+            button3.TabIndex = 12;
+            button3.Text = "Изход";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 461);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label5;
+        private Label label6;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ 2grupa/Zadachi-12b-2grupa/Zadacha01/Form1.cs
@@ -0,0 +1,52 @@
+﻿namespace Zadacha01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ClearData();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ActiveForm.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ClearData();
+        }
+
+        private void ClearData()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            label5.Text = "";
+            label6.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var x = int.Parse(textBox1.Text);
+            var y = double.Parse(textBox2.Text);
+            var z = int.Parse(textBox3.Text);
+            var worker = int.Parse(textBox4.Text);
+            var grapes = x * y;
+            var wine = 0.4 * grapes / 2.5;
+            if (wine >= z)
+            {
+                var leftWine = wine - z;
+                label5.Text = $"Good harvest this year! Total wine: {Math.Floor(wine)} liters.";
+                label6.Text = $"{Math.Ceiling(leftWine)} liters left -> {Math.Ceiling(leftWine / worker)} liters per person.";
+            }
+            else
+            {
+                label5.Text = $"It will be a tough winter! More {Math.Floor(z - wine)} liters wine needed.";
+            }
+
+        }
+    }
+}
